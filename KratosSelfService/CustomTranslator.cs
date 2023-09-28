6e7fce8d98085d1ecd37927365ec34dbacb5e2f3
Joscha <34318751+josxha@users.noreply.github.com>
2023-09-28 17:23:23 +0200

    clean up, add l10n, add login view

---
+++ KratosSelfService/CustomTranslator.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.Localization;
+
+namespace KratosSelfService;
+
+public class CustomTranslator(IStringLocalizer<CustomTranslator> localizer) : ICustomTranslator
+{
+    public string GetTranslation(string text)
+    {
+        return Localizer[text];
+    }
+
+    private IStringLocalizer<CustomTranslator> Localizer { get;  } = localizer;
+}
+
+public interface ICustomTranslator
+{
+    string GetTranslation(string text);
+}
--- KratosSelfService/Program.cs
+++ KratosSelfService/Program.cs
@@ -1,26 +1,11 @@
-using KratosSelfService.Components;
+using KratosSelfService;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Configuration.AddEnvironmentVariables().Build(); // use environment variables
 
-// Add services to the container.
-builder.Services.AddRazorComponents()
-    .AddServerComponents();
-
+var startup = new Startup(builder.Configuration, builder.Environment);
+startup.ConfigureServices(builder.Services);
 var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseStaticFiles();
-
-app.MapRazorComponents<App>()
-    .AddServerRenderMode();
+await startup.Configure(app);
 
 app.Run();

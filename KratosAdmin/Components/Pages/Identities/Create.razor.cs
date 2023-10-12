afa56a259fece5d6596f86b6186c772ba1096144
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-12 12:28:54 +0200

    style improvements, begin of create page

--- KratosAdmin/Components/Pages/Identities/Create.razor.cs
+++ KratosAdmin/Components/Pages/Identities/Create.razor.cs
@@ -1,4 +1,5 @@
-﻿using KratosAdmin.Services;
+﻿using KratosAdmin.Models;
+using KratosAdmin.Services;
 using Microsoft.AspNetCore.Components;
 
 namespace KratosAdmin.Components.Pages.Identities;
@@ -6,10 +7,27 @@ namespace KratosAdmin.Components.Pages.Identities;
 public partial class Create
 {
     private bool _isLoading = true;
+
+    private List<string>? _schemaIds;
+    private string? _selectedSchema;
+    private List<TraitsSchemaData>? _traitsSchemas;
     [Inject] private ApiService ApiService { get; set; } = default!;
+    [Inject] private IdentitySchemaService SchemaService { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
+        _schemaIds = await SchemaService.ListIds();
+        await OnSchemaSelect(_schemaIds.First());
         _isLoading = false;
+    }
+
+    private async Task OnSchemaSelect(string schemaId)
+    {
+        _selectedSchema = schemaId;
+        _traitsSchemas = await SchemaService.GetTraitSchemas(_selectedSchema);
+    }
+
+    private async Task SubmitForm()
+    {
     }
 }

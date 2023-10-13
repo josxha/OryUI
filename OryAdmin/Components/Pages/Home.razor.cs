1b28a9f79d482c092b76acc59364abc47da5e00e
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-13 23:29:43 +0200

    improve OryAdmin design

--- OryAdmin/Components/Pages/Home.razor.cs
+++ OryAdmin/Components/Pages/Home.razor.cs
@@ -1,34 +1,35 @@
-﻿using Ory.Kratos.Client.Api;
-using Ory.Kratos.Client.Client;
+﻿using KratosAdmin.Services;
+using Microsoft.AspNetCore.Components;
 
 namespace KratosAdmin.Components.Pages;
 
 public partial class Home
 {
     private string? _kratosAdminUrl;
+    private bool _kratosAlive;
     private string? _kratosPublicUrl;
-    private string? _kratosReady;
+    private bool _kratosReady;
     private string? _kratosVersion;
+    [Inject] private ApiService ApiService { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
-        var metadataApi = new MetadataApi(new Configuration
-        {
-            BasePath = EnvService.KratosAdminUrl
-        });
         _kratosPublicUrl = EnvService.KratosPublicUrl;
         _kratosAdminUrl = EnvService.KratosAdminUrl;
         try
         {
-            var version = await metadataApi.GetVersionAsync();
+            var version = await ApiService.Metadata.GetVersionAsync();
             _kratosVersion = version._Version;
-            var ready = await metadataApi.IsReadyAsync();
-            _kratosReady = ready.Status;
+            var alive = await ApiService.Metadata.IsAliveAsync();
+            _kratosAlive = alive.Status == "ok";
+            var ready = await ApiService.Metadata.IsReadyAsync();
+            _kratosReady = ready.Status == "ok";
         }
         catch (Exception exception)
         {
             _kratosVersion = exception.Message;
-            _kratosReady = exception.Message;
+            _kratosAlive = false;
+            _kratosReady = false;
         }
     }
 }

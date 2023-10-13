ad53ded4afc52572b59f8e0611cff5a62e143938
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-13 20:36:31 +0200

    implement settings page, update l18n

---
+++ KratosSelfService/Extensions/GroupEnumExt.cs
@@ -0,0 +1,19 @@
+﻿using Ory.Kratos.Client.Model;
+
+namespace KratosSelfService.Extensions;
+
+public static class GroupEnumExt
+{
+    public static string ToLocalString(this KratosUiNode.GroupEnum instance, ICustomTranslator translator)
+    {
+        return instance switch
+        {
+            KratosUiNode.GroupEnum.Password => translator.GetTranslation("Change Password"),
+            KratosUiNode.GroupEnum.Profile => translator.GetTranslation("Profile Settings"),
+            KratosUiNode.GroupEnum.Totp => translator.GetTranslation("Manage 2FA TOTP Authenticator App"),
+            KratosUiNode.GroupEnum.LookupSecret => translator.GetTranslation("Manage 2FA Backup Recovery Codes"),
+            KratosUiNode.GroupEnum.Webauthn => translator.GetTranslation("WebAuthn Login"),
+            _ => instance.ToString()
+        };
+    }
+}

e7af8fb5b18e9fab106040e5b54a50f00a740017
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-16 20:15:03 +0200

    add identity schema tabs

--- OryAdmin/Components/Pages/Identities/Schemas/Index.razor.cs
+++ OryAdmin/Components/Pages/Identities/Schemas/Index.razor.cs
@@ -8,14 +8,33 @@ public partial class Index
 {
     private bool _isLoading = true;
 
-    private List<KratosIdentitySchemaContainer> _schemas;
-    private string? _selectedSchema;
+    private List<KratosIdentitySchemaContainer>? _schemas;
+
+    private object? _selectedSchema;
+
+    [SupplyParameterFromQuery(Name = "id")]
+    // ReSharper disable once UnusedAutoPropertyAccessor.Local
+    private string? SchemaId { get; set; }
+
     [Inject] private ApiService ApiService { get; set; } = default!;
     [Inject] private IdentitySchemaService SchemaService { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
         _schemas = await ApiService.KratosIdentity.ListIdentitySchemasAsync();
+        if (_schemas.Count == 0)
+        {
+            _isLoading = false;
+            return;
+        }
+
+        if (SchemaId == null)
+        {
+            Navigation.NavigateTo($"identities/schemas?id={_schemas.First().Id}");
+            return;
+        }
+
+        _selectedSchema = _schemas.First(schema => schema.Id == SchemaId);
         _isLoading = false;
     }
 }

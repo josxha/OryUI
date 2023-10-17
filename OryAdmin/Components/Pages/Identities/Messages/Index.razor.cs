9c619570b0f348491209bb29dfd2acc68eba18c3
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-17 17:27:46 +0200

    add messages index and titles, extract pagination, add page titles

---
+++ OryAdmin/Components/Pages/Identities/Messages/Index.razor.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Ory.Kratos.Client.Model;
+using OryAdmin.Services;
+
+namespace OryAdmin.Components.Pages.Identities.Messages;
+
+public partial class Index
+{
+    private bool _isLoading = true;
+    private List<KratosMessage> _messages;
+
+    [SupplyParameterFromQuery(Name = "page")]
+    private int? PageNr { get; set; }
+
+    [Inject] private IdentitySchemaService SchemaService { get; set; } = default!;
+    [Inject] private ApiService ApiService { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        PageNr ??= 1;
+
+        // TODO use pagination to support a large amount
+        _messages = await ApiService.KratosCourier.ListCourierMessagesAsync();
+
+        _isLoading = false;
+    }
+
+    private void RefreshPage(MouseEventArgs arg)
+    {
+        Navigation.Refresh(true);
+    }
+}
---
+++ OryAdmin/Components/Pages/Identities/Messages/View.razor.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Components;
+using Ory.Kratos.Client.Model;
+using OryAdmin.Services;
+
+namespace OryAdmin.Components.Pages.Identities.Messages;
+
+public partial class View
+{
+    private bool _isLoading = true;
+    private KratosMessage? _message;
+    [Parameter] public string? MessageId { get; set; }
+    [Inject] private ApiService ApiService { get; set; } = default!;
+
+    protected override async Task OnInitializedAsync()
+    {
+        _message = await ApiService.KratosCourier.GetCourierMessageAsync(MessageId);
+        _isLoading = false;
+    }
+}

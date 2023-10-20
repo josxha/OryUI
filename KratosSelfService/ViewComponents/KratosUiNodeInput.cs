a94dde69b060206c2049eaa587b5a8325c9b7db3
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-20 23:58:06 +0200

    split KratosUiNodeInput view into multiple views

--- KratosSelfService/ViewComponents/KratosUiNodeInput.cs
+++ KratosSelfService/ViewComponents/KratosUiNodeInput.cs
@@ -6,8 +6,42 @@ namespace KratosSelfService.ViewComponents;
 
 public class KratosUiNodeInput : ViewComponent
 {
-    public async Task<ViewViewComponentResult> InvokeAsync(KratosUiNode text)
+    public async Task<ViewViewComponentResult> InvokeAsync(KratosUiNode node)
     {
-        return View("Default", text);
+        switch (node.Type)
+        {
+            case KratosUiNode.TypeEnum.Text:
+                return View("Text", node);
+            case KratosUiNode.TypeEnum.Input:
+                switch (node.Attributes.GetKratosUiNodeInputAttributes().Type)
+                {
+                    case KratosUiNodeInputAttributes.TypeEnum.Text:
+                    case KratosUiNodeInputAttributes.TypeEnum.Password:
+                    case KratosUiNodeInputAttributes.TypeEnum.Number:
+                    case KratosUiNodeInputAttributes.TypeEnum.Hidden:
+                    case KratosUiNodeInputAttributes.TypeEnum.Email:
+                    case KratosUiNodeInputAttributes.TypeEnum.Tel:
+                    case KratosUiNodeInputAttributes.TypeEnum.DatetimeLocal:
+                    case KratosUiNodeInputAttributes.TypeEnum.Date:
+                    case KratosUiNodeInputAttributes.TypeEnum.Url:
+                        return View("InputField", node);
+                    case KratosUiNodeInputAttributes.TypeEnum.Checkbox:
+                        return View("InputCheckbox", node);
+                    case KratosUiNodeInputAttributes.TypeEnum.Submit:
+                        return View("InputSubmit", node);
+                    case KratosUiNodeInputAttributes.TypeEnum.Button:
+                        return View("InputButton", node);
+                    default:
+                        return View("InputDefault", node);
+                }
+            case KratosUiNode.TypeEnum.Img:
+                return View("Img", node);
+            case KratosUiNode.TypeEnum.A:
+                return View("Anchor", node);
+            case KratosUiNode.TypeEnum.Script:
+                return View("Script", node);
+            default:
+                return View("Default", node);
+        }
     }
 }

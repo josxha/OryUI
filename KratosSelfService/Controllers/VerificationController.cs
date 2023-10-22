88e4923e36f2752cb6938301dc4331d012f78e54
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-22 16:57:06 +0200

    port verification flow

--- KratosSelfService/Controllers/VerificationController.cs
+++ KratosSelfService/Controllers/VerificationController.cs
@@ -1,5 +1,7 @@
 using KratosSelfService.Services;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Ory.Kratos.Client.Client;
 using Ory.Kratos.Client.Model;
 
@@ -8,12 +10,19 @@ namespace KratosSelfService.Controllers;
 public class VerificationController(ILogger<VerificationController> logger, ApiService api) : Controller
 {
     [HttpGet("verification")]
-    public async Task<IActionResult> Verification([FromQuery(Name = "flow")] string? flowId)
+    public async Task<IActionResult> Verification(
+        [FromQuery(Name = "flow")] string? flowId,
+        [FromQuery(Name = "return_to")] string? returnTo,
+        [FromQuery(Name = "message")] string? jsonMessages)
     {
         if (flowId == null)
         {
             // initiate flow
-            var url = api.GetUrlForBrowserFlow("verification");
+            logger.LogDebug("No flow ID found in URL query initializing login flow");
+            var url = api.GetUrlForBrowserFlow("verification", new Dictionary<string, string?>
+            {
+                ["return_to"] = returnTo
+            });
             return Redirect(url);
         }
 
@@ -24,8 +33,22 @@ public class VerificationController(ILogger<VerificationController> logger, ApiS
         }
         catch (ApiException exception)
         {
-            Console.WriteLine(exception.Message);
-            throw;
+            logger.LogDebug("Could not retrieve flow for flow ID: {Message}", exception.Message);
+            var url = api.GetUrlForBrowserFlow("verification", new Dictionary<string, string?>
+            {
+                ["return_to"] = returnTo
+            });
+            return Redirect(url);
+        }
+
+        // check for custom messages in the query string
+        if (string.IsNullOrWhiteSpace(jsonMessages))
+        {
+            var messages = JsonConvert.DeserializeObject<List<KratosUiText>>(jsonMessages);
+            if (messages != null)
+            {
+                flow.Ui.Messages.AddRange(messages);
+            }
         }
 
         return View("Verification", flow);

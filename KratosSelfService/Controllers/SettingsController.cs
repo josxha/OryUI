3a62dff97ab21b82af6f8ae721a30876d6433e10
Joscha <34318751+josxha@users.noreply.github.com>
2023-10-22 16:43:30 +0200

    add return_to to settings endpoint

--- KratosSelfService/Controllers/SettingsController.cs
+++ KratosSelfService/Controllers/SettingsController.cs
@@ -1,22 +1,42 @@
 ﻿using KratosSelfService.Models;
 using KratosSelfService.Services;
 using Microsoft.AspNetCore.Mvc;
+using Ory.Kratos.Client.Client;
+using Ory.Kratos.Client.Model;
 
 namespace KratosSelfService.Controllers;
 
-public class SettingsController(ApiService api) : Controller
+public class SettingsController(ILogger<SettingsController> logger, ApiService api) : Controller
 {
     [HttpGet("settings")]
-    public async Task<IActionResult> Settings([FromQuery(Name = "flow")] string? flowId)
+    public async Task<IActionResult> Settings(
+        [FromQuery(Name = "flow")] string? flowId,
+        [FromQuery(Name = "return_to")] string? returnTo
+    )
     {
         if (flowId == null)
         {
+            // init new flow
+            logger.LogDebug("No flow ID found in URL query initializing login flow");
             var newFlow = await api.Frontend
-                .CreateBrowserSettingsFlowAsync(cookie: Request.Headers.Cookie);
+                .CreateBrowserSettingsFlowAsync(cookie: Request.Headers.Cookie, returnTo: returnTo);
             return Redirect($"settings?flow={newFlow.Id}");
         }
 
-        var flow = await api.Frontend.GetSettingsFlowAsync(flowId, cookie: Request.Headers.Cookie);
+        KratosSettingsFlow flow;
+        try
+        {
+            flow = await api.Frontend.GetSettingsFlowAsync(flowId, cookie: Request.Headers.Cookie);
+        }
+        catch (ApiException exception)
+        {
+            // create new flow
+            logger.LogDebug("Exception while retrieving settings flow: {Message}", exception.Message);
+            var newFlow = await api.Frontend
+                .CreateBrowserSettingsFlowAsync(cookie: Request.Headers.Cookie, returnTo:returnTo);
+            return Redirect($"settings?flow={newFlow.Id}");
+        }
+
         var model = new SettingsModel(flow, api.GetSettingsUrl(flow.Id));
         return View("Settings", model);
     }

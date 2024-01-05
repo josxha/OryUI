ee901f14596ae30ea89332b3f2bb5997f9f1f731
Joscha <34318751+josxha@users.noreply.github.com>
2024-01-06 00:37:58 +0100

    add schema dropdown on create user page

--- OryAdmin/Components/Pages/Identities/Users/Create.razor.cs
+++ OryAdmin/Components/Pages/Identities/Users/Create.razor.cs
@@ -10,37 +10,42 @@ namespace OryAdmin.Components.Pages.Identities.Users;
 
 public partial class Create
 {
-    private readonly JObject _json = new();
+    private JObject _json = new();
     private string? _errorMessage;
     private bool _isLoading = true;
+    private bool _schemaDropdownActive;
 
     private List<string>? _schemaIds;
-    private string? _selectedSchema;
     private List<TraitsSchemaData>? _traitsSchemas;
 
     [SupplyParameterFromQuery(Name = "schema")]
-    private string? SchemaId { get; set; }
+    private string? SelectedSchemaId { get; set; }
 
     [Inject] private ApiService ApiService { get; set; } = default!;
     [Inject] private IdentitySchemaService SchemaService { get; set; } = default!;
 
     protected override async Task OnInitializedAsync()
     {
-        _schemaIds = await SchemaService.ListIds();
-        SchemaId ??= _schemaIds.First();
-        await OnSchemaSelect(SchemaId);
+        _schemaIds ??= await SchemaService.ListIds();
+        SelectedSchemaId ??= _schemaIds.First();
         _isLoading = false;
     }
 
-    private async Task OnSchemaSelect(string schemaId)
+    protected override async Task OnParametersSetAsync()
     {
-        _selectedSchema = schemaId;
-        _traitsSchemas = await SchemaService.GetTraitSchemas(_selectedSchema);
+        _schemaIds ??= await SchemaService.ListIds();
+        SelectedSchemaId ??= _schemaIds.First();
+        _traitsSchemas = await SchemaService.GetTraitSchemas(SelectedSchemaId);
+    }
+
+    private void OnSelectionChanged()
+    {
+        _schemaDropdownActive = false;
     }
 
     private async Task SubmitForm()
     {
-        var body = new KratosCreateIdentityBody(schemaId: _selectedSchema, traits: _json);
+        var body = new KratosCreateIdentityBody(schemaId: SelectedSchemaId, traits: _json);
         try
         {
             _ = await ApiService.KratosIdentity.CreateIdentityAsync(body);
